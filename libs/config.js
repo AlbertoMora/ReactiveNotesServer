24a88eff2e31f80837d399eb8896b07c439885a6
amora95 <albertoamp95@gmail.com>
2019-12-29 23:44:53 -0600

    set port to work with heroku

--- libs/config.js
+++ libs/config.js
@@ -1,6 +1,6 @@
 const enviroment = require('./server-enviroment')();
 const path = require('path');
-const port = normalizePort(process.env.PORT || '3000');
+const port = process.env.PORT || '3000';
 
 module.exports = (app, express) => {
     //settings
@@ -11,20 +11,4 @@ module.exports = (app, express) => {
     app.set('views', path.join(viewsPath));
     app.set('view engine', 'pug');
 }
-
-function normalizePort(val) {
-    const port = parseInt(val, 10);
-  
-    if (isNaN(port)) {
-      // named pipe
-      return val;
-    }
-  
-    if (port >= 0) {
-      // port number
-      return port;
-    }
-  
-    return false;
-  }
   
